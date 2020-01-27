4647d0b821c8cb504b6edb585f4b2e8792e35a18
ashish <ashish@DESKTOP-OHR0F1U>
2020-01-27 19:09:27 +0530

    commit

---
+++ FaceCompare/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FaceCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Store";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 181);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(250, 24);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(1098, 596);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(768, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 632);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FaceCompare/Form1.cs
@@ -0,0 +1,119 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FaceCompare
+{
+    public partial class Form1 : Form
+    {
+        private Emgu.CV.Capture cap;
+        List<Image<Gray, byte>> imageparts = new List<Image<Gray, byte>>();
+        string faceRecordFolder = Environment.CurrentDirectory + "\\FaceRecords\\";
+        private Dictionary<string, Image<Gray, Byte>> facesAvailable = new Dictionary<string, Image<Gray, byte>>();
+        HaarCascade _cascadeClassifier = new HaarCascade("haarcascades\\haarcascade_frontalface_alt.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+            cap = new Emgu.CV.Capture(0);
+            LoadImageData();
+            StartScanning();
+        }
+
+        private void LoadImageData()
+        {
+            var files = Directory.EnumerateFiles(faceRecordFolder, "*.*", SearchOption.TopDirectoryOnly)
+                        .Where(s => Path.GetExtension(s).ToLowerInvariant() == ".bmp").ToList();
+
+            facesAvailable.Clear();
+
+            files.ForEach(filePath =>
+            {
+                facesAvailable.Add(Path.GetFileName(filePath), new Image<Gray, byte>(filePath));
+            });
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> img = new Image<Gray, byte>(pictureBox1.ImageLocation);
+
+            MCvAvgComp[][] rectangles = img.DetectHaarCascade(_cascadeClassifier);
+
+            foreach (var ract in rectangles[0])
+            {
+                img.ROI = ract.rect;
+                imageparts.Add(img.Copy());
+            }
+
+            int x = 0;
+            Directory.CreateDirectory(faceRecordFolder);
+            foreach (var imgs in imageparts)
+            {
+                var imgss = imgs.Resize(100, 100, INTER.CV_INTER_CUBIC);
+                imgss.Save(faceRecordFolder + "\\" + textBox1.Text + "(" + x + ").bmp");
+                x++;
+                if (x > 10) break;
+            };
+        }
+
+        private void StartScanning()
+        {
+            Application.Idle += new EventHandler(delegate (object senders, EventArgs ee)
+            {
+                var mat = cap.QueryFrame();
+
+                Bitmap bitmap = mat.Bitmap;
+
+                Image<Gray, byte> img = new Image<Gray, byte>(bitmap);
+
+                MCvAvgComp[][] rectangles = img.DetectHaarCascade(_cascadeClassifier, 1.2, 0, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new System.Drawing.Size(20, 20));
+
+                foreach (var rect in rectangles[0])
+                {
+                    mat.Draw(rect.rect, new Bgr(255, 0, 0), 2);
+                    img = new Image<Gray, byte>(bitmap);
+                    string name = FaceRecognition(img);
+                    MCvFont mCvFont = new MCvFont(FONT.CV_FONT_HERSHEY_PLAIN, 20, 20);
+                    label1.Text = name;
+                    //break;
+                }
+
+                imageBox1.Image = mat;
+
+            });
+        }
+
+        private string FaceRecognition(Image<Gray, byte> img)
+        {
+            if (facesAvailable.Count != 0)
+            {
+                MCvTermCriteria termCrit = new MCvTermCriteria(facesAvailable.Count, 0.001);
+
+                EigenObjectRecognizer recognizer = new EigenObjectRecognizer(facesAvailable.Select(x => x.Value).ToArray(),
+                    facesAvailable.Select(x => x.Key).ToArray(),
+                    1000, ref termCrit);
+
+                var name = recognizer.Recognize(img.Resize(100, 100, INTER.CV_INTER_CUBIC));
+                return Regex.Replace(name.Split('.')[0], "\\(.+?\\)", "");
+            }
+            return "";
+        }
+    }
+}
